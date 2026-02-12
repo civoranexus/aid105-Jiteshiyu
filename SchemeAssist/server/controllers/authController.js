8e24e5bb9f785afed3d0607c007650fed4756f6a
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2026-02-12 23:23:55 +0530

    Update authController.js

--- SchemeAssist/server/controllers/authController.js
+++ SchemeAssist/server/controllers/authController.js
@@ -1,6 +1,5 @@
 const User = require("../models/User");
 
-const AppError = require("../utils/AppError");
 const catchAsync = require("../utils/catchAsync");
 
 const DEFAULT_EMAIL = "testuser@example.com";
